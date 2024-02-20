9bf9437051e5c3498667572b771de997d8af1db4
Miho Moriki <miho.moriki@rakus-partners.co.jp>
2024-02-20 12:09:35 +0900

    確認できたので消去

--- src/main/java/com/example/exintermediatenumbersearch/ExIntermediateNumberSearchApplication.java
+++ src/main/java/com/example/exintermediatenumbersearch/ExIntermediateNumberSearchApplication.java
@@ -8,7 +8,6 @@ public class ExIntermediateNumberSearchApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExIntermediateNumberSearchApplication.class, args);
-		System.out.println("pushの確認");
 	}
 
 }
